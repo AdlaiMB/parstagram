70e026e2d822f7012456e7b1f2b5fd99e0418cbd
Adlai M.B <adlaimb@gmail.com>
2021-10-14 11:45:49 -0700

    set up login method for a user

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -2,11 +2,18 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -42,5 +49,24 @@ public class LoginActivity extends AppCompatActivity {
     private void LoginUser(String username, String password){
         Log.i(TAG, "Attempting to login user " + username);
 
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+
+                if (e != null){
+
+                    Log.e(TAG,"Issue with login");
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this,"Success",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity(){
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
