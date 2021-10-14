a864ff0042014b2bf1ad1bc83e103c67c418872e
Adlai M.B <adlaimb@gmail.com>
2021-10-14 16:39:53 -0700

    create sign up button and create a method to signup a user with a onclick listener

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -14,6 +14,7 @@ import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSingUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSingUp = findViewById(R.id.btnSignup);
 
         // Set up a button listener
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +52,39 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btnSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG,"On click for signup");
+
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                createNewUser(username,password);
+            }
+        });
+
+    }
+
+    private void createNewUser(String username, String password){
+
+        ParseUser newUser = new ParseUser();
+        newUser.setUsername(username);
+        newUser.setPassword(password);
+
+        newUser.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+
+                if(e != null){
+                    Log.e(TAG, "Could not create user");
+                    Toast.makeText(LoginActivity.this,"Unsuccessful creation of account.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this,"Success",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void LoginUser(String username, String password){
